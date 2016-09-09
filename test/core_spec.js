c6460682c06afee3f60a4ee73bce9f9e4b68fe6b
Ilya Muravev <ilya.yarn@gmail.com>
2016-09-10 03:18:54 +0400

    -

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
-import {List, Map} from 'immutable';
-import {expect} from 'chai';
-import {setEntries, next, vote} from '../src/core'
+import {List, Map} from "immutable";
+import {expect} from "chai";
+import {setEntries, next, vote} from "../src/core";
 
 describe('application logic', () => {
 
@@ -30,6 +30,48 @@ describe('application logic', () => {
                     $$pair: List.of('Trainspotting', '28 Days Later')
                 }),
                 $$entries: List.of('Sunshine')
+            }));
+        });
+
+        it('puts winner of current vote back to entries', () => {
+            const $$state = Map({
+                $$vote: Map({
+                    $$pair: List.of('Trainspotting', '28 Days Later'),
+                    $$tally: Map({
+                        'Trainspotting': 4,
+                        '28 Days Later': 2
+                    })
+                }),
+                $$entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const $$nextState = next($$state);
+
+            expect($$nextState).to.be.equal(Map({
+                $$vote: Map({
+                    $$pair: List.of('Sunshine', 'Millions')
+                }),
+                $$entries: List.of('127 Hours', 'Trainspotting')
+            }));
+        });
+
+        it('puts both from tied vote back to entries', () => {
+            const $$state = Map({
+                $$vote: Map({
+                    $$pair: List.of('Trainspotting', '28 Days Later'),
+                    $$tally: Map({
+                        'Trainspotting': 3,
+                        '28 Days Later': 3
+                    })
+                }),
+                $$entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const $$nextState = next($$state);
+
+            expect($$nextState).to.be.equal(Map({
+                $$vote: Map({
+                    $$pair: List.of('Sunshine', 'Millions')
+                }),
+                $$entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
             }));
         });
     });
