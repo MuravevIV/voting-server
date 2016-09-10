e2270af10156e398810c17eb78f94d7eddd8771e
Ilya Muravev <ilya.yarn@gmail.com>
2016-09-10 04:04:53 +0400

    -

--- test/core_spec.js
+++ test/core_spec.js
@@ -96,47 +96,35 @@ describe('application logic', () => {
     describe('vote', () => {
 
         it('creates a tally for the voted entry', () => {
-            const $$state = Map({
-                $$vote: Map({
-                    $$pair: List.of('Trainspotting', '28 Days Later')
-                }),
-                $$entries: List()
+            const $$voteState = Map({
+                $$pair: List.of('Trainspotting', '28 Days Later')
             });
-            const $$nextState = vote($$state, 'Trainspotting');
+            const $$nextVoteState = vote($$voteState, 'Trainspotting');
 
-            expect($$nextState).to.be.equal(Map({
-                $$vote: Map({
-                    $$pair: List.of('Trainspotting', '28 Days Later'),
-                    $$tally: Map({
-                        'Trainspotting': 1
-                    })
-                }),
-                $$entries: List()
+            expect($$nextVoteState).to.be.equal(Map({
+                $$pair: List.of('Trainspotting', '28 Days Later'),
+                $$tally: Map({
+                    'Trainspotting': 1
+                })
             }));
         });
 
         it('adds to existing tally for the voted entry', () => {
-            const $$state = Map({
-                $$vote: Map({
-                    $$pair: List.of('Trainspotting', '28 Days Later'),
-                    $$tally: Map({
-                        'Trainspotting': 3,
-                        '28 Days Later': 2
-                    })
-                }),
-                $$entries: List()
+            const $$voteState = Map({
+                $$pair: List.of('Trainspotting', '28 Days Later'),
+                $$tally: Map({
+                    'Trainspotting': 3,
+                    '28 Days Later': 2
+                })
             });
-            const $$nextState = vote($$state, 'Trainspotting');
+            const $$nextVoteState = vote($$voteState, 'Trainspotting');
 
-            expect($$nextState).to.be.equal(Map({
-                $$vote: Map({
-                    $$pair: List.of('Trainspotting', '28 Days Later'),
-                    $$tally: Map({
-                        'Trainspotting': 4,
-                        '28 Days Later': 2
-                    })
-                }),
-                $$entries: List()
+            expect($$nextVoteState).to.be.equal(Map({
+                $$pair: List.of('Trainspotting', '28 Days Later'),
+                $$tally: Map({
+                    'Trainspotting': 4,
+                    '28 Days Later': 2
+                })
             }));
         });
     });
